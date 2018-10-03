cf0398ce69ec8a32050853556b6a67fc8c64adf1
will <will@will-MS-7693>
2018-10-03 12:16:13 -0400

    update 1.2 for main and control
    iterator given a class, instructions class, comand structure
    established, basic command list layed out, just needs the corresponding
    codes to be added to Controls class.

--- src/Controls.java
+++ src/Controls.java
@@ -1,3 +1,13 @@
+import java.util.concurrent.TimeUnit;
+
+
+/*
+ * For actions that require a list of actions we can handle it by first;
+ * creating a string to hold the list of actions. 
+ * then adding each action to that list and returning it
+ * the start  command marker (D1) and end marker (DF) keep things in order
+ */
+
 
 public class Controls {
 
@@ -5,45 +15,50 @@ public class Controls {
 
 	}
 
-	public void  checkSensors(){ //a constant pool of calls to the sensors
-
+	public static String  checkSensors(){ //a constant pool of calls to the sensors
+		return "";
 	}
 
-	public void swtch(int number) { //this method will control the switches on the track
-
+	public static String  switchTrack(int number) { //this method will control the switches on the track
+		return "";
 	}
 
-	public void Accelerate(int number){ //method to handle train acceleration, number is 
+	public static String  Accelerate(int number){ //method to handle train acceleration, number is 
 		//the adjusted speed value, check to make sure that 
 		//the adjustment is in fact an increase and not a decrease. 
+		return "";
+	}
+
+	public static String  Decelerate() { //method to handle train deceleration
+		return"";
+	}
+
+	public static String Go() { //Tells the train to start moving
+		return"";
+	}
+
+	public static String Stop() { //Tells the train to stop
+		return"";
+	}
+
+	public static String Reverse() {
+		return "";
+	}
+	
+	public static void curveDecel() { //Has the train slow depending on the severity of the curve. 
 
 	}
 
-	public void Decelerate() { //method to handle train deceleration
-
-	}
-
-	public void Go() { //Tells the train to start moving
-
-	}
-
-	public void Stop() { //Tells the train to stop
-
-	}
-
-	public void curveDecel() { //Has the train slow depending on the severity of the curve. 
-
-	}
-
-	public void tmpTimeDelay() {
-
+	public static void timeDelay(int min, int sec) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(sec);
+		TimeUnit.MINUTES.sleep(min);
 	}
 
 
-	public static float howFastAmI(String Name) {
+	public static String howFastAmI(String Name) {
 
-
-		return 0;
+		return"";
+	
 	}
 
 }
