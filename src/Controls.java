4323cd209f40289c6fa2969c6f5890cf53967578
will <will@will-MS-7693>
2018-10-04 01:36:49 -0400

    update made to trainPtr
    rounded out the train pointer object class, added specified fields from
    Command Ref doc. Also added setters and getters where necessary

--- src/Controls.java
+++ src/Controls.java
@@ -1,7 +1,15 @@
 import java.util.concurrent.TimeUnit;
 
-
 /*
+ *  control types;
+ *  type:	description:
+ *  0		cab-1
+ *  1		TMCC    ************ our train engine is legacy
+ *  2		Legacy   our tracks currently are legacy 
+ *  3		R100
+ */
+
+/* 
  * For actions that require a list of actions we can handle it by first;
  * creating a string to hold the list of actions. 
  * then adding each action to that list and returning it
@@ -9,44 +17,74 @@ import java.util.concurrent.TimeUnit;
  */
 
 
-public class Controls {
+public class Controls{
 
-	public static void init(trainPtr t) {
+//	public static void init(trainPtr t) {
+//
+//	}
 
-	}
-
-	public static String  checkSensors(){ //a constant pool of calls to the sensors
+	public static String checkSensors(){ //a constant pool of calls to the sensors
+		
 		return "";
 	}
 
 	public static String  switchTrack(int number) { //this method will control the switches on the track
-		return "";
+		return ""+number+"";
 	}
 
-	public static String  Accelerate(int number){ //method to handle train acceleration, number is 
-		//the adjusted speed value, check to make sure that 
-		//the adjustment is in fact an increase and not a decrease. 
-		return "";
+	public static String  Accelerate(int number, trainPtr t){ //method to handle train acceleration, number is 
+		if(t.getDirection()== "L") { //the train is facing left, left facing data packet
+			return "";
+		}
+		
+		else { // the train is facing right, right facing data packet
+			return "";
+		} 
 	}
 
-	public static String  Decelerate() { //method to handle train deceleration
+	public static String  Decelerate(trainPtr t) { //method to handle train deceleration
+		if(t.getDirection()== "L") { //the train is facing left, left facing data packet
+			return "";
+		}
+		
+		else { // the train is facing right, right facing data packet
+			return "";
+		}
+	}
+
+	public static String Go(trainPtr t) { //Tells the train to start moving
+		
+		if(t.getDirection()== "L") { //the train is facing left, left facing data packet
+			return "";
+		}
+		
+		else { // the train is facing right, right facing data packet
+			return "";
+		}
+	}
+
+	public static String Stop(trainPtr t) { //Tells the train to stop
 		return"";
 	}
 
-	public static String Go() { //Tells the train to start moving
-		return"";
-	}
-
-	public static String Stop() { //Tells the train to stop
-		return"";
-	}
-
-	public static String Reverse() {
-		return "";
+	public static String Reverse(trainPtr t) {
+		if(t.getDirection()== "L") { //the train is facing left, left facing data packet
+			return "";
+		}
+		
+		else { // the train is facing right, right facing data packet
+			return "";
+		}
 	}
 	
-	public static void curveDecel() { //Has the train slow depending on the severity of the curve. 
-
+	public static String curveDecel(trainPtr t) { //Has the train slow depending on the severity of the curve. 
+		if(t.getDirection()== "L") { //the train is facing left, left facing data packet
+			return "";
+		}
+		
+		else { // the train is facing right, right facing data packet
+			return "";
+		}
 	}
 
 	public static void timeDelay(int min, int sec) throws InterruptedException {
@@ -55,7 +93,7 @@ public class Controls {
 	}
 
 
-	public static String howFastAmI(String Name) {
+	public static String howFastAmI(trainPtr t) {
 
 		return"";
 	
--- src/trainPtr.java
+++ src/trainPtr.java
@@ -1,12 +1,233 @@
 
 public class trainPtr{
+
+	// below variables come from locomotive data found on LCS PDI Command Ref.pdf page 17 ________________
 	
+	static String locoTyp; // 0 - 15, see page 13 of LCS PDI Command Ref.pdf
+	static String conTyp; // Cab1, TMCC, R100, Legacy
+	static String sndSysTyp; // NONE, RS, RSS, RSL
+	static String clsSpec; // Engine Class Specification
+	static String TSDBLeft; //Cab-2 ICONS on lower left soft key
+	static String TSDBRight; //Cab-2 ICONS on lower right soft key
+	static String runLvl; //Diesel Run Level
+	static String lbrBias; // Loco Labor Adjustment Bias
+	static String spdLim; // Speed limit setting
+	static String mxSpd; // Max speed setting
+	static String smkLvl; // Smoke level, Off, L, M, H
+	static String ditLit; // Ditch Light operation control
+	static String trnBrk; // Train break setting
+
+	// ___________________________________________________________________________________________________
+	
+	static String name;
+	static String location;
+	static String direction;
+	static int size;
+
+
 	public trainPtr(String Name, int Size, String Location, String Direction) { //direction will be either right or left
-		String name = Name;
-		String location = Location;
-		String direction = Direction;
-		int size = Size;
+		this.name = Name;
+		this.location = Location;
+		this.direction = Direction;
+		this.size = Size;
+	}
+
+	public static String getName() {
+		return name;
+	}
+
+	public static void location() {
+		//TODO 
+		//this method will be updated later once the map has been built
+	}
+
+	public static String getDirection() {
+		return direction;
+	}
+
+	public static int getSize() {
+		return size;
 	}
 	
-	
+	/**
+	 * @return the locoTyp
+	 */
+	public static String getLocoTyp() {
+		return locoTyp;
+	}
+
+	/**
+	 * @param locoTyp the locoTyp to set
+	 */
+	public static void setLocoTyp(String locoTyp) {
+		trainPtr.locoTyp = locoTyp;
+	}
+
+	/**
+	 * @return the conTyp
+	 */
+	public static String getConTyp() {
+		return conTyp;
+	}
+
+	/**
+	 * @param conTyp the conTyp to set
+	 */
+	public static void setConTyp(String conTyp) {
+		trainPtr.conTyp = conTyp;
+	}
+
+	/**
+	 * @return the sndSysTyp
+	 */
+	public static String getSndSysTyp() {
+		return sndSysTyp;
+	}
+
+	/**
+	 * @param sndSysTyp the sndSysTyp to set
+	 */
+	public static void setSndSysTyp(String sndSysTyp) {
+		trainPtr.sndSysTyp = sndSysTyp;
+	}
+
+	/**
+	 * @return the clsSpec
+	 */
+	public static String getClsSpec() {
+		return clsSpec;
+	}
+
+	/**
+	 * @param clsSpec the clsSpec to set
+	 */
+	public static void setClsSpec(String clsSpec) {
+		trainPtr.clsSpec = clsSpec;
+	}
+
+	/**
+	 * @return the tSDBLeft
+	 */
+	public static String getTSDBLeft() {
+		return TSDBLeft;
+	}
+
+	/**
+	 * @param tSDBLeft the tSDBLeft to set
+	 */
+	public static void setTSDBLeft(String tSDBLeft) {
+		TSDBLeft = tSDBLeft;
+	}
+
+	/**
+	 * @return the tSDBRight
+	 */
+	public static String getTSDBRight() {
+		return TSDBRight;
+	}
+
+	/**
+	 * @param tSDBRight the tSDBRight to set
+	 */
+	public static void setTSDBRight(String tSDBRight) {
+		TSDBRight = tSDBRight;
+	}
+
+	/**
+	 * @return the runLvl
+	 */
+	public static String getRunLvl() {
+		return runLvl;
+	}
+
+	/**
+	 * @param runLvl the runLvl to set
+	 */
+	public static void setRunLvl(String runLvl) {
+		trainPtr.runLvl = runLvl;
+	}
+
+	/**
+	 * @return the lbrBias
+	 */
+	public static String getLbrBias() {
+		return lbrBias;
+	}
+
+	/**
+	 * @param lbrBias the lbrBias to set
+	 */
+	public static void setLbrBias(String lbrBias) {
+		trainPtr.lbrBias = lbrBias;
+	}
+
+	/**
+	 * @return the spdLim
+	 */
+	public static String getSpdLim() {
+		return spdLim;
+	}
+
+	/**
+	 * @param spdLim the spdLim to set
+	 */
+	public static void setSpdLim(String spdLim) {
+		trainPtr.spdLim = spdLim;
+	}
+
+	/**
+	 * @return the mxSpd
+	 */
+	public static String getMxSpd() {
+		return mxSpd;
+	}
+
+	/**
+	 * @param mxSpd the mxSpd to set
+	 */
+	public static void setMxSpd(String mxSpd) {
+		trainPtr.mxSpd = mxSpd;
+	}
+
+	/**
+	 * @return the smkLvl
+	 */
+	public static String getSmkLvl() {
+		return smkLvl;
+	}
+
+	/**
+	 * @param smkLvl the smkLvl to set
+	 */
+	public static void setSmkLvl(String smkLvl) {
+		trainPtr.smkLvl = smkLvl;
+	}
+
+	/**
+	 * @return the ditLit
+	 */
+	public static String getDitLit() {
+		return ditLit;
+	}
+
+	/**
+	 * @param ditLit the ditLit to set
+	 */
+	public static void setDitLit(String ditLit) {
+		trainPtr.ditLit = ditLit;
+	}
+
+	/**
+	 * @return the trnBrk
+	 */
+	public static String getTrnBrk() {
+		return trnBrk;
+	}
+
+	/**
+	 * @param trnBrk the trnBrk to set
+	 */
+	public static void setTrnBrk(String trnBrk) {
+		trainPtr.trnBrk = trnBrk;
+	}
 }
